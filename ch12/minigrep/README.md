925e3654634b47a51db205427cbe74c3281badc8
Donald Sebastian Leung <donaldsebleung@gmail.com>
2022-08-29 22:30:18 +0800

    Complete minigrep exercise in chapter 12

--- ch12/minigrep/README.md
+++ ch12/minigrep/README.md
@@ -4,7 +4,7 @@ A miniature version of grep, derived from the code presented in chapter 12 of th
 
 ### Tasks
 
-- [ ] Allow case (in)sensitivity to be specified as a command-line argument as well
-- [ ] Ensure the command-line argument switch takes precedence over `IGNORE_CASE` environment variable in controlling case (in)sensitivity
-- [ ] Further refine the error message when incorrect (number of) arguments are specified
-- [ ] Allow the flag for case-insensitivity to be specified out-of-order
+- [x] Allow case (in)sensitivity to be specified as a command-line argument as well
+- [x] Ensure the command-line argument switch takes precedence over `IGNORE_CASE` environment variable in controlling case (in)sensitivity
+- [x] Further refine the error message when incorrect (number of) arguments are specified
+- [x] Allow the flag for case-insensitivity to be specified out-of-order
--- ch12/minigrep/src/main.rs
+++ ch12/minigrep/src/main.rs
@@ -1,20 +1,56 @@
-use std::env;
-use std::process;
-
-mod runner;
-use runner::Config;
+use clap::Parser;
+use envy;
+use serde::Deserialize;
+use std::{error::Error, fs, process};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args = Args::parse();
+    let env = envy::from_env::<Env>().unwrap_or(Env { ignore_case: None });
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {err}");
-        process::exit(1);
-    });
-
-    if let Err(e) = runner::run(config) {
+    if let Err(e) = run(args, env) {
         eprintln!("Application error: {e}");
-
         process::exit(1);
     }
+}
+
+/// A miniature version of grep
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Substring to query for
+    #[clap(value_parser)]
+    query: String,
+
+    /// Path to file with contents to query over
+    #[clap(value_parser)]
+    file_path: String,
+
+    /// Perform a case-insensitive query
+    #[clap(short, long, action = clap::ArgAction::SetTrue)]
+    ignore_case: bool,
+}
+
+#[derive(Deserialize)]
+struct Env {
+    ignore_case: Option<bool>,
+}
+
+fn run(args: Args, env: Env) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(args.file_path)?;
+
+    let results = minigrep::search(
+        &args.query,
+        &contents,
+        args.ignore_case || env.ignore_case.unwrap_or(false),
+    );
+
+    if results.len() == 0 {
+        process::exit(1);
+    }
+
+    for line in results {
+        println!("{line}");
+    }
+
+    Ok(())
 }
